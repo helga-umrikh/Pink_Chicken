442942a7f23e48c0cc090e0755fa124fdbe68a39
helga-umrikh <zetaumr@gmail.com>
2023-12-19 05:16:04 +0300

    feat: add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './index.scss'
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import MainPage  from "./pages/MainPage/MainPage"
+
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Routes>
+                <Route path="/" element={MainPage} />
+            </Routes>
         </div>
     )
 }
