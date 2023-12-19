2726f3ed1c5456f7ff21178b73c850d953bb9787
helga-umrikh <zetaumr@gmail.com>
2023-12-19 11:42:31 +0300

    feat: add layout header PersonalAssistant component

--- src/components/Files/Files.jsx
+++ src/components/Files/Files.jsx
@@ -8,7 +8,7 @@ const Files = () => {
             <p className=" files__title typogr_title">Файлы</p>
             <div className='files__start-content'>
                 <div className="files__image">
-                    <img src={files} alt="image of files" />
+                    <img src={files} alt="files icon" />
                 </div>
                 <p className="typogr_descr">
                     Закажи у личного помощника медиаплан. Он появится в этом
--- src/components/PersonalAssistant/PersonalAssistant.jsx
+++ src/components/PersonalAssistant/PersonalAssistant.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from 'react'
 import './PersonalAssistant.scss'
 
 const PersonalAssistant = () => {
-  return (
-    <div className='pers-assistant'>PersonalAssistant</div>
-  )
+    return (
+        <div className="pers-assistant">
+            <div className='pers-assistant__header'>
+                <div className="pers-assistant__header_info">
+                    <p className="typogr_title">Личный помощник</p>
+                    <div className="pers-assistant__acc-bar">
+                        <p className="pers-assistant__user-name typogr_sub">
+                            Jim Davidson
+                        </p>
+                        <p className="typogr_addit">Jim Davidson@adaurum.ru</p>
+                    </div>
+                </div>
+                <button className="pers-assistant__drop-down-btn">
+                    <div className="pers-assistant__drop-down-btn_container">
+                        <figure className="pers-assistant__drop-down-btn_dots"></figure>
+                        <figure className="pers-assistant__drop-down-btn_dots"></figure>
+                        <figure className="pers-assistant__drop-down-btn_dots"></figure>
+                    </div>
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default PersonalAssistant
